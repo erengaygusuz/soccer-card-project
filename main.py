002433887ddb9e8deb8feef4d9ed31ae93c1a34b
Eren Gaygusuz <gaygusuzeren@gmail.com>
2022-12-22 11:06:36 +0300

    Adding libraries and writing get_team_names function

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import requests
+import random
+import pathlib
+import urllib.request
+import os.path
+from lxml import etree
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
+playerNames = []
+playerNos = []
+playerImageLinks = []
+teamNames = []
+teamIds = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_team_names():
 
+    leagueLinkAddress = 'https://www.transfermarkt.com.tr/super-lig/startseite/wettbewerb/TR1'
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    leagueTreeResponse = requests.get(leagueLinkAddress, headers=headers)
+    soupLeague = BeautifulSoup(leagueTreeResponse.content, 'html.parser')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    teamNamesTree = (etree.HTML(str(soupLeague)).xpath('/html/body/div[2]/main/div[2]/div[1]/div[2]/div[2]/div/table/tbody/tr'))
+
+    for i in range(len(teamNamesTree)):
+
+        if teamNamesTree[i].xpath('td[2]/a')[0].text != '':
+            teamNames.append(teamNamesTree[i].xpath('td[2]/a/@href')[0].split('/')[1])
+            teamIds.append(teamNamesTree[i].xpath('td[2]/a/@href')[0].split('/')[4])
+
+def main():
+    get_team_names()
+
+    teamColors = [[0 for i in range(2)] for j in range(len(teamNames))]
+    playerInfos = [[str(), list(), list(), list(), list(), list(), list()] for j in range(len(teamNames))]
+
+if __name__ == "__main__":
+    main()
